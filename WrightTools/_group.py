f34eaea420aec36e675b65c64412d1621d757a97
ksunden <sunden@wisc.edu>
2018-08-18 00:02:04 -0500

    Remove subclass

--- WrightTools/_group.py
+++ WrightTools/_group.py
@@ -4,4 +4,4 @@ class Group():
     def __init__(self):
         weakref.finalize(self, self.close)
     def close(self):
-        from .collection import Collection
+        from . import collection
--- WrightTools/collection.py
+++ WrightTools/collection.py
@@ -1,5 +1,2 @@
-from ._group import Group
 from collections.abc import Iterator
 isinstance(lambda: 1, Iterator)
-class Collection(Group):
-    pass
