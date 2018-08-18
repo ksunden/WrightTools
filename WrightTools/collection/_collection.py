5962cee82b7f88c5cf45f7fad31c45978b18e786
ksunden <sunden@wisc.edu>
2018-08-17 21:16:21 -0500

    Bypass skimage

--- WrightTools/collection/_collection.py
+++ WrightTools/collection/_collection.py
@@ -9,7 +9,7 @@ import numpy as np
 from .. import exceptions as wt_exceptions
 from .._group import Group
 
-import skimage
+import dask.array as da
 
 
 # --- define --------------------------------------------------------------------------------------
