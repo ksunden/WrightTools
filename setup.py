4195bdcfb1f2064ce2c48e02a9df07916b064c1c
ksunden <sunden@wisc.edu>
2018-08-18 00:22:05 -0500

    remove coverage report

--- setup.py
+++ setup.py
@@ -1,53 +1,20 @@
 #! /usr/bin/env python3
 
-import os
 from setuptools import setup, find_packages
-
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-
-def read(fname):
-    return open(os.path.join(here, fname)).read()
-
-
-extra_files = {
-    "WrightTools": [
-        "datasets",
-        "datasets/*",
-        "datasets/*/*",
-        "datasets/*/*/*",
-        "datasets/*/*/*/*",
-        "CITATION",
-        "VERSION",
-        "WT5_VERSION",
-    ]
-}
 
 version = "0.0.0"
 
 setup(
     name="WrightTools",
     packages=find_packages(exclude=("tests", "tests.*")),
-    package_data=extra_files,
     python_requires=">=3.5",
     setup_requires=["pytest-runner"],
     tests_require=[
         "pytest",
-        "pytest-cov",
     ],
-    extras_require={
-        "docs": ["sphinx", "sphinx-gallery==0.1.12", "sphinx-rtd-theme"],
-        "dev": ["black", "pre-commit", "pydocstyle"],
-    },
     version=version,
-    description="Tools for loading, processing, and plotting multidimensional spectroscopy data.",
-    long_description=read("README.rst"),
     author="WrightTools Developers",
     license="MIT",
-    url="http://wright.tools",
-    keywords="spectroscopy science multidimensional visualization",
-    entry_points={"console_scripts": ["wt-tree=WrightTools.__main__:wt_tree"]},
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Science/Research",
