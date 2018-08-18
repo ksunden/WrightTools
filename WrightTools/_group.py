eee9e968a2dfce0459f8d7de219be4a4a299387c
ksunden <sunden@wisc.edu>
2018-08-17 23:11:15 -0500

    Strip out instances dict

--- WrightTools/_group.py
+++ WrightTools/_group.py
@@ -1,10 +1,7 @@
 import weakref
 from . import kit as wt_kit
 class Group():
-    _instances = {}
     def __init__(self, file=None, parent=None, name=None, **kwargs):
-        Group._instances[id(self)] = self
         weakref.finalize(self, self.close)
-
     def close(self):
         from .collection import Collection
