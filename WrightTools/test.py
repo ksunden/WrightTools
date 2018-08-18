732d4f53ef43150ede8f7ee56cbb63cbf1eabee0
Kyle Sunden <sunden@wisc.edu>
2018-08-18 13:31:53 -0500

    Strip out unittest

--- WrightTools/test.py
+++ WrightTools/test.py
@@ -1,7 +1,3 @@
 import WrightTools as wt
-import unittest
-class TestFoo(unittest.TestCase):
-    def test_foo(self):
-        wt.Foo()
-if __name__ == "__main__":
-    unittest.main()
+def test_foo():
+    wt.Foo()
