61c30439068b4b81d741c0c4f92558f1a5206cd9
ksunden <sunden@wisc.edu>
2018-08-17 21:35:58 -0500

    Strip down kit to only be timestamp

--- WrightTools/kit/__init__.py
+++ WrightTools/kit/__init__.py
@@ -1,11 +1,4 @@
 """General-purpose tool kit."""
 # flake8: noqa
 
-from ._array import *
-from ._calculate import *
-from ._discover_dimensions import *
-from ._list import *
-from ._path import *
 from ._timestamp import *
-from ._unicode import *
-from ._utilities import *
