7ecc33364d52f5c2a6eb3170a6eb7987e9a7f232
ksunden <sunden@wisc.edu>
2018-08-17 23:28:49 -0500

    simplify imports and test case

--- WrightTools/__init__.py
+++ WrightTools/__init__.py
@@ -1,2 +1,1 @@
 from . import collection
-from .collection import *
--- tests/base.py
+++ tests/base.py
@@ -1,21 +1,4 @@
-"""Test basic instantiation and handling."""
-
-
-# --- import --------------------------------------------------------------------------------------
-
-
-import os
-
 import WrightTools as wt
-
-
-# --- test ----------------------------------------------------------------------------------------
-
-
 def test_named_root_collection():
-    c = wt.Collection(name="blaise")
-
-def test_parent_child():
-    parent = wt.Collection(name="mother")
-    child = wt.Collection(parent=parent, name="goose")
+    c = wt.collection.Collection(name="blaise")
 
